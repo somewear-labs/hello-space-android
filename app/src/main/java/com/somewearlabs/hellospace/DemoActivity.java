b1f6f06c0b307e6bde938250978093d2eb4ea840
Braden Young <bnyoung11@gmail.com>
2019-03-07 11:01:22 -0800

    Checkin demo

--- app/src/main/java/com/somewearlabs/hellospace/DemoActivity.java
+++ app/src/main/java/com/somewearlabs/hellospace/DemoActivity.java
@@ -1,8 +1,13 @@
 package com.somewearlabs.hellospace;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -13,9 +18,9 @@ import android.widget.TextView;
 
 import com.somewearlabs.somewearcore.api.DeviceConnectionState;
 import com.somewearlabs.somewearcore.api.DevicePayload;
+import com.somewearlabs.somewearcore.api.FirmwareUpdateStatus;
 import com.somewearlabs.somewearcore.api.SomewearDevice;
 import com.somewearlabs.somewearcore.api.SomewearDeviceCallback;
-import com.somewearlabs.uisupport.api.SomewearUI;
 
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
@@ -26,11 +31,10 @@ import java.util.Locale;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
 
-public class MainActivity extends AppCompatActivity {
+public class DemoActivity extends Activity {
 
     private SomewearDevice device = SomewearDevice.getInstance();
     private SomewearDeviceCallback deviceCallback = device.callback();
-    private SomewearUI somewearUI = SomewearUI.getInstance();
     private CompositeDisposable disposable = new CompositeDisposable();
     private List<String> payloadEvents = new ArrayList<>();
 
@@ -40,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button scanButton = findViewById(R.id.scanButton);
-        scanButton.setOnClickListener(v -> somewearUI.toggleScan(this));
+        scanButton.setOnClickListener(v -> toggleScan());
 
         Button sendButton = findViewById(R.id.sendButton);
         sendButton.setOnClickListener(v -> sendMessage());
@@ -54,11 +58,11 @@ public class MainActivity extends AppCompatActivity {
         eventsRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         eventsRecyclerView.setAdapter(eventsAdapter);
 
-        // Display firmware update dialogs as required
-        somewearUI.configureFirmwareUpdateHandling(this);
-
         // Observe payload changes
         deviceCallback.registerAsPayloadListener(this::didReceivePayload);
+
+        // Observe Firmware update status changes
+        deviceCallback.registerAsFirmwareUpdateListener(this::didReceiveFirmwareUpdate);
 
         disposable.addAll(
                 // Observe connectivity changes
@@ -96,7 +100,9 @@ public class MainActivity extends AppCompatActivity {
                         .observeOn(AndroidSchedulers.mainThread())
                         .subscribe(activityState -> {
                             activityTextView.setText(getString(R.string.activity_state_text_view, activityState));
-                        })
+                        }),
+
+                device.getFirmwareUpdateStatus().subscribe()
         );
     }
 
@@ -123,17 +129,87 @@ public class MainActivity extends AppCompatActivity {
         addEvent("id=" + payload.getParcelId() + "; status=" + payload.getStatus());
     }
 
+    private void didReceiveFirmwareUpdate(FirmwareUpdateStatus status) {
+        switch (status) {
+            case None:
+                break;
+            case Available:
+                presentFirmwareUpdateAvailableDialog();
+                break;
+            case Required:
+                presentFirmwareUpdateRequiredDialog();
+                break;
+        }
+    }
+
     private void addEvent(String eventMessage) {
         String event = String.format(Locale.getDefault(), "%1$tH:%1$tM:%1$tS %2$s", new Date(), eventMessage);
         payloadEvents.add(0, event);
         eventsAdapter.notifyItemInserted(0);
     }
 
+    private void toggleScan() {
+        deviceCallback.toggleScan(result -> {
+            switch(result) {
+                case Success:
+                    break;
+                case BleUnsupported:
+                    // if the device doesn't have BLE, we can't use Somewear and the user shouldn't be here anyway
+                    break;
+                case BleDisabled:
+                    Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                    startActivity(intent);
+                    break;
+                case LocationPermissionRequired:
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        requestPermissions(new String[] { Manifest.permission.ACCESS_COARSE_LOCATION }, 50);
+                    }
+                    break;
+            }
+        });
+    }
+
+    private void presentFirmwareUpdateAvailableDialog() {
+        new AlertDialog.Builder(this)
+                .setTitle("Update Available")
+                .setMessage("Your Somewear hotspot has an update available. Update to get the most out your device!")
+                .setPositiveButton("Update", (dialog, which) -> startFirmwareUpdate())
+                .setNegativeButton("Cancel", (dialog, which) -> {}) // ignore
+                .create()
+                .show();
+    }
+
+    private void presentFirmwareUpdateRequiredDialog() {
+        new AlertDialog.Builder(this)
+                .setTitle("Update Required")
+                .setMessage("A critical software update is required for your Somewear hotspot. You cannot use your Somewear hotspot until this update is installed.")
+                .setPositiveButton("Update", (dialog, which) -> startFirmwareUpdate())
+                .setNegativeButton("Disconnect", (dialog, which) -> deviceCallback.disconnect())
+                .create()
+                .show();
+    }
+
+    private void startFirmwareUpdate() {
+        deviceCallback.startFirmwareUpdate(result -> {
+            switch (result) {
+                case Success:
+                    break;
+                case Busy:
+                    break;
+                case DeviceDisconnected:
+                case Error:
+                    // try again before this?
+                    deviceCallback.disconnect();
+                    break;
+            }
+        });
+    }
+
     private RecyclerView.Adapter eventsAdapter = new RecyclerView.Adapter() {
         @NonNull
         @Override
         public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-            return EventViewHolder.build(parent);
+            return DemoActivity.EventViewHolder.build(parent);
         }
 
         @Override
@@ -154,12 +230,11 @@ public class MainActivity extends AppCompatActivity {
             super(itemView);
         }
 
-        static EventViewHolder build(ViewGroup parent) {
+        static DemoActivity.EventViewHolder build(ViewGroup parent) {
             View v = LayoutInflater
                     .from(parent.getContext())
                     .inflate(android.R.layout.simple_list_item_1, parent, false);
-            return new EventViewHolder(v);
+            return new DemoActivity.EventViewHolder(v);
         }
     }
 }
-
